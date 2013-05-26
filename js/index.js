803a719240fb6549ad23ddc10d1a9f1ea0b0bd16
Egils Vernavs <eagle@apollo.lv>
2013-05-26 23:15:26 +0300

    param param

--- js/index.js
+++ js/index.js
@@ -1,64 +1,114 @@
-var balls = [];
-var ball_cnt = 10;
+var balls = {};
+var ball_counter = 0;
+var balls_size = 0;
+var ball_cnt = 3;
 var radius = 32;
 var ratio = 10000;
 var points = 0;
 var appw = 0, apph = 0;
 var gamew = 0, gameh = 0, offsetLeft = 0, offsetTop = 0;
 var interval;
+var game_time;
+var time_milisec = 6000;
+var milisec_gone = 0;
+var addBallInterval;
     
-ball = function(index){
-    this.x = getRandom($("#app").offset().left, appw+$("#app").offset().left - (radius * 2));
-    this.y = getRandom($("#app").position().top, apph+$("#app").position().top - (radius * 2));
+var ball = function(index){
+	this.x = getRandom($("#app").offset().left, appw+$("#app").offset().left - (radius * 2));
+	this.y = getRandom($("#app").position().top, apph+$("#app").position().top - (radius * 2));
     this.color = "#FF0000";
     this.speedx = 0.2 + Math.random() * 5;
     this.speedy = 0.2 + Math.random() * 5;
     this.radius = radius;
-    this.name = "ball"+index;
     this.ball = document.createElement("div");
 }
 
+var setUpNewBall = function(){
+	if(balls_size < 5)
+	{	
+		ball_counter++;
+		balls_size++;
+		balls[ball_counter] = new ball();
+		$("#app").append(balls[ball_counter].ball);
+		var img = document.createElement("img");
+			img.src = "./images/ball.png";
+			img.style.width = (radius * 2)+"px";
+			img.style.height = (radius * 2)+"px";
+		balls[ball_counter].ball.id = ball_counter;
+		balls[ball_counter].ball.style.position = "absolute";
+		balls[ball_counter].ball.appendChild(img);
+		balls[ball_counter].ball.img = img;
+		balls[ball_counter].ball.style.left = (balls[ball_counter].x)+"px";
+		balls[ball_counter].ball.style.top = (balls[ball_counter].y)+"px";
+		balls[ball_counter].ball.onmousedown = ballClick;
+	}
+}
+
 var move = function(){
-	
 	for(var key in balls)
 	{
-		balls[key].x += balls[key].speedx;
-		balls[key].y += balls[key].speedy;
-		//if ball hits bottom or top side of screen
-		if(balls[key].y + (2 * radius) >= apph + $("#app").position().top 
-				|| balls[key].y <= $("#app").position().top)
-		{
-			balls[key].speedy *= -1;
-		}
-		//if ball hits right or left side of screen
-		if(balls[key].x + (2* radius) >= appw + $("#app").position().left 
-				|| balls[key].x <= $("#app").position().left)
-		{
-			balls[key].speedx *= -1;
-		}
+		balls[key].x = getRandom($("#app").offset().left, appw+$("#app").offset().left - (radius * 2));
+		balls[key].y = getRandom($("#app").position().top, apph+$("#app").position().top - (radius * 2));
 		balls[key].ball.style.left = balls[key].x+"px";
 		balls[key].ball.style.top = balls[key].y+"px";
-		
 		if((parseInt(key)+1) === balls.length)
 		{
 			clearInterval(interval);
-			interval = setInterval(move, 50);
+			interval = setInterval(move, 3000);
 		}
 	}
 } 
 
 function getRandom (min, max) {
-    return Math.random() * (max - min) + min;
+    return (Math.random() * (max - min) + min);
 }
 
 function ballClick(e){
-    var index = e.target.parentNode.id.split("_");
-    $(e.target).remove();
-    delete(balls[index[1]]);
+	//some bug workaround
+	if(e.target.parentNode.id != "app")
+	{
+		console.log("Delete ", e.target.parentNode);
+		balls_size--;
+		var id = e.target.parentNode.id;
+		$(e.target.parentNode).remove();
+		delete(balls[id]);
+		points++;
+		$("#points").text(points);
+	}
+}
+
+timeTick = function(){
+	milisec_gone++;
+	var time_left = time_milisec - milisec_gone;
+	if(time_left > 0)
+	{
+		var date = new Date(time_left * 10);
+		var milisec = date.getMilliseconds();
+		var sec = date.getSeconds();
+		var min = date.getMinutes();
+		$("#time").text(min+" : "+sec+" : "+milisec);
+	}
+	else
+	{
+		$("#time").text("00 : 00 : 000");
+		clearInterval(interval);
+		clearInterval(addBallInterval);
+		clearInterval(game_time);
+		for(var key in balls)
+		{
+			$("#"+balls[key].ball.id).remove();
+			delete(balls[key]);
+		}
+		balls_size = 0;
+		$("#game").hide();
+		$("#end").show();
+		console.log("Tavs rezultâts - savâktas",$("#points").text(),"futbola bumbas!");
+	}
 }
 
 $(document).ready(function(){
 	$("#container").hide();
+	$("#end").hide();
 	$(window).resize(onResize);
 	$("#app").css ("height",$("#app").width());
 	setTimeout(function(){
@@ -76,31 +126,36 @@ $(document).ready(function(){
 		$("#game").show();
         for(var i = ball_cnt; i > 0; i--)
         {
-            balls.push(new ball());
-            $("#app").append(balls[balls.length - 1].ball);
-            var img = document.createElement("img");
-                img.src = "./images/ball.png";
-                img.style.width = (radius * 2)+"px";
-                img.style.height = (radius * 2)+"px";
-            balls[balls.length - 1].ball.id = "ball_"+(balls.length - 1);
-            balls[balls.length - 1].ball.style.position = "absolute";
-            balls[balls.length - 1].ball.appendChild(img);
-            balls[balls.length - 1].ball.img = img;
-            balls[balls.length - 1].ball.style.left = 
-                 (balls[balls.length - 1].x)+"px";
-            balls[balls.length - 1].ball.style.top = 
-                 (balls[balls.length - 1].y)+"px";
-            balls[balls.length - 1].ball.onclick = ballClick;
+			setUpNewBall();
         }
 		
-        interval = setInterval(move,50);
+        interval = setInterval(move,3000);
         $("#pagewrap").hide();
+		game_time = setInterval(timeTick,10);
+		addBallInterval = setInterval(setUpNewBall, 500);
     });
+	
+	$("#end").click(function()
+	{
+		$("#game").show();
+		$("#end").hide();
+		points = 0;
+		$("#points").text(points);
+		balls = {};
+		ball_counter = 0;
+		ball_size = 0;
+		milisec_gone = 0;
+		interval = setInterval(move,3000);
+		game_time = setInterval(timeTick,10);
+		addBallInterval = setInterval(setUpNewBall, 500);
+		for(var i = ball_cnt; i > 0; i--)
+        {
+			setUpNewBall();
+        }
+	});
 });
 
-onResize = function()
-{
-	$("#made_by").html($("html").width());
+onResize = function(){
 	$("#app").css ("height",$("#app").width());
 	m_top = $("#container").height() / 2;
 	$("#container").css("margin-top","-"+m_top+"px");
@@ -110,7 +165,6 @@ onResize = function()
 	var oldOfLeft = offsetLeft;
 	var oldgamew = gamew;
     var oldgameh = gameh;
-	console.log(oldgamew,$("html").width());
     if(oldw != $("#app").width() || oldh != $("#app").height() || oldgamew != $("html").width() || oldgameh != $("html").height())
     {
         appw = $("#app").width();
@@ -120,7 +174,6 @@ onResize = function()
 		offsetTop = $("#app").offset().top;
 		offsetLeft = $("#app").offset().left;
         radius = Math.floor((appw * apph) / ratio);
-        console.log("resize from",oldw,"x",oldh,"to", appw, "x",apph, "radius:",radius);
         for(var key in balls)
         {
             balls[key].radius = radius;
