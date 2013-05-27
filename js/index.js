021d6c63a6da61b140bcab7c9ce1e939577923a9
Sintija <pelnenasintija@inbox.lv>
2013-05-27 13:21:00 +0300

    test

--- js/index.js
+++ js/index.js
@@ -1,18 +1,25 @@
-var balls = {};
-var ball_counter = 0;
-var balls_size = 0;
-var ball_cnt = 5;
-var radius = 32;
-var ratio = 10000;
-var points = 0;
-var appw = 0, apph = 0;
-var gamew = 0, gameh = 0, offsetLeft = 0, offsetTop = 0;
-var interval;
-var game_time;
-var time_milisec = 6000;
-var milisec_gone = 0;
-var addBallInterval;
-    
+var balls = {}; //objekts kur glabâsies bumbiòas
+var ball_counter = 0; //bumbiòu identifikatora skaitîtâjs skaitîtâjs
+var balls_size = 0; //bumbiòu objekta izmçrs - cik bumbiòas ir objektâ
+var ball_cnt = 5; //sâkuma bumbiòu skaits
+var radius = 32; //sâkuma bumbiòu râdiuss
+var ratio = 10000; //bumbiòas un ekrâna attiecîba - 1bumbiòa = 1/10000 daïa no ekrâna platuma * ekrâna garuma
+var points = 0; //punkti
+var appw = 0, apph = 0; //bumbiòu laukuma izmçrs
+var gamew = 0, gameh = 0, offsetLeft = 0, offsetTop = 0; //spçles loga izmçrs un tâ nobîdes
+var interval; //intervâls pçc kura mainâs visu bumbiòu novietojums
+var game_time; //spçles laika intervâla mainîgais
+var time_milisec = 6000; //1 spçles laiks milisekundçs
+var milisec_gone = 0; //pagâjuðâs milisekundes
+var addBallInterval; //bumbiòu pievienoðanas intervâla mainîgais
+  
+  
+/*objekta bumbiòa konstruktora funkcija
+	x un y argumenti tiek aprçíinâti pçc nejauðîbas principa, ar noteikumu, ka bumbiòas koordinâtâm jâatbilst bumbiòu laukuma izmçriem
+	color - krâsa
+	speedx , speedy - âtrums pçc nejauðîbas principa pareizinâts ar konstanti un pieskaitîts 0.2, lai bumbiòas âtrums nevarçtu bût 0
+	ball nepiecieðamais html elements, kurâ attçlo bumbiòu
+*/ 	  
 var ball = function(index){
 	this.x = getRandom($("#app").offset().left, appw+$("#app").offset().left - (radius * 2));
 	this.y = getRandom($("#app").position().top, apph+$("#app").position().top - (radius * 2));
@@ -23,6 +30,9 @@ var ball = function(index){
     this.ball = document.createElement("div");
 }
 
+/*
+	funkcija, kas pievieno jaunu bumbiòu spçlei
+*/
 var setUpNewBall = function(){
 	if(balls_size < 10)
 	{	
@@ -44,6 +54,9 @@ var setUpNewBall = function(){
 	}
 }
 
+/*
+	funkcija, kas pârvieto bumbiòas
+*/
 var move = function(){
 	for(var key in balls)
 	{
@@ -59,15 +72,21 @@ var move = function(){
 	}
 } 
 
+/*
+	funkcija, kas atgrieþ nejauðu skaitli robeþâs no - lîdz
+*/
 function getRandom (min, max) {
     return (Math.random() * (max - min) + min);
 }
 
+/*
+	funkcija, kas uz notikumu onmousedown izpilda bumbiòu dzçðanu no ekrâna un objekta un pieskaita lietoâjam punktus
+*/
 function ballClick(e){
 	//some bug workaround
 	if(e.target.parentNode.id != "app")
 	{
-		console.log("Delete ", e.target.parentNode);
+		/*console.log("Delete ", e.target.parentNode);*/
 		balls_size--;
 		var id = e.target.parentNode.id;
 		$(e.target.parentNode).remove();
@@ -77,6 +96,9 @@ function ballClick(e){
 	}
 }
 
+/*
+	spçles laika skaitîðanas funkcija, parâda lietotâjam atlikuðo spçles laiku un ja laiks beidzies izvada rezultâtu
+*/
 timeTick = function(){
 	milisec_gone++;
 	var time_left = time_milisec - milisec_gone;
@@ -101,25 +123,29 @@ timeTick = function(){
 		}
 		balls_size = 0;
 		showEndScreen($("#points").text());
-		console.log("Tavs rezultâts - savâktas",$("#points").text(),"futbola bumbas!");
 	}
 }
 
+/*
+	funkcija, kas parâda diva 'end_screen' saturu, kad spele ir beigusies
+*/
 function showEndScreen(points) {
 
-	$('#end_screen').reveal({ // The item which will be opened with reveal
+	$('#end_screen').reveal({
 		animation: 'none',                   // fade, fadeAndPop, none
-		animationspeed: 600,                       // how fast animtions are
-		closeonbackgroundclick: false,              // if you click background will modal close?
-		dismissmodalclass: 'close'    // the class of a button or element that will close an open modal
+		animationspeed: 600,                      
+		closeonbackgroundclick: false,        
+		dismissmodalclass: 'close'
 	});
 	$("#end_result").html(points);
 }
 
+/*
+	JQUERY funkcija, kas nostrâdâ brîdî, kad tîmekïa vietnes lapa ielâdçjusies
+*/
 $(document).ready(function(){
 	$("#container").hide();
 	$(window).resize(onResize);
-	$("#app").css ("height",$("#app").width());
 	setTimeout(function(){
 		m_top = $("#container").height() / 2;
 		$("#container").css("margin-top","-"+m_top+"px");
@@ -128,22 +154,27 @@ $(document).ready(function(){
 	
     $("#pixel").text(screen.width+"x"+screen.height);
     $("#game").hide();
+	//funcijas, kas nostrâdâ uz pogas 'SPÇLÇT' klikðíi
     $("#btn_start").click(function()
     {
-        
         onResize();
 		$("#game").show();
         for(var i = ball_cnt; i > 0; i--)
         {
 			setUpNewBall();
         }
-		
+		//izsauc funkciju move ik pçc 3 sekundçm
         interval = setInterval(move,3000);
         $("#pagewrap").hide();
+		//izsauc funkciju timeTick ik pçc 10 mikrosekundçm
 		game_time = setInterval(timeTick,10);
+		//izsauc funkciju setUpNewBall ik pçc pus sekundes
 		addBallInterval = setInterval(setUpNewBall, 500);
     });
 	
+	/*
+		tiek atstatîti spçles dati, lai sâktu spçli no jauna(tiek izpildîts pçc pogas 'play_again' nospieðanas)
+	*/
 	$("#play_again").click(function()
 	{
 		$("#game").show();
@@ -162,6 +193,9 @@ $(document).ready(function(){
         }
 	});
 	
+	/*
+		tiek atvçrts aplikâcijas pirmais skats(tiek izpildîts pçc pogas 'to_home' nospieðanas)
+	*/
 	$("#to_home").click(function()
 	{
 		points = 0;
@@ -174,6 +208,9 @@ $(document).ready(function(){
 		$("#game").hide();
 	});
 	
+	/*
+		parâdâs apraksts par spçli (popup)
+	*/
 	$("#about").click(function()
 	{
 		$('#info_screen').reveal({ // The item which will be opened with reveal
@@ -185,8 +222,25 @@ $(document).ready(function(){
 	});
 });
 
+/*
+	funkcija, kas maina bumbiòu izmçrus un novietojumu atkarîbâ no daþâdâm ekrâna izmaiòâm 
+*/
 onResize = function(){
+	$("#app_div").css ("height",$("#app_div").width());
 	$("#app").css ("height",$("#app").width());
+	
+	move();
+	if($("html").height()>$("html").width())
+	{
+		$("#info").css({'float': 'none', 'width': '100%', 'margin': '0 auto', 'padding-top': '0px'});
+		$("#app_div").css({'float': 'none', 'width': '100%'});
+	}
+	else
+	{
+		$("#info").css({'float': 'left', 'width': '25%', 'margin': 'auto', 'padding-top': '100px'});
+		$("#app_div").css({'float': 'right', 'width': '75%'});
+	}
+	
 	m_top = $("#container").height() / 2;
 	$("#container").css("margin-top","-"+m_top+"px");
     var oldw = appw;
